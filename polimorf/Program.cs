ab32bbdc0a5f6f3b4add6dd77b41f6f4a731b83e
kcelig <112395678+kcelig@users.noreply.github.com>
2022-08-30 09:19:55 +0200

    Add files via upload

---
+++ polimorf/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace inheritance
+{
+    //polimorfizam
+
+    class Osoba {                           //bazna klasa
+        public string ime { get; set; }
+        public string prezime { get; set; }
+
+        public override string ToString()
+        {
+           // return this.ime + " " + this.prezime;
+            return base.ToString();
+        }
+    }
+
+    class Ucenik : Osoba {
+        public int razred { get; set; }
+   
+        public override string ToString() {
+            return this.ime + " " + this.prezime + " " + this.razred;
+            
+        }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Osoba o = new Osoba();
+            o.ime = "Ivana";
+            o.prezime = "Brlić Mažuranić";
+
+            Ucenik u = new Ucenik();
+            u.ime = "Antun Gustav";
+            u.prezime = "Matoš";
+            u.razred = 3;
+
+            Console.WriteLine("Osoba: {0}", o.ToString());
+            Console.WriteLine("Učenik: {0}", u.ToString());
+
+
+          
+
+            Console.ReadKey();
+
+        }
+    }
+}
